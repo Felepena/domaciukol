70405158c42bd337e1f0e7fb9330227a0b7982b1
filma <fil.masilko@gmail.com>
2023-12-12 11:13:59 +0100

    dobre rano

---
+++ fortnite.js
@@ -0,0 +1,31 @@
+
+    let table = document.createElement("table");
+
+    for (let i = 0; i < 11; i++) {
+        let tr = document.createElement("tr")
+        table.appendChild(tr)
+
+        for (let j = 0; j < 4 ; j++) {
+
+
+
+        }
+        for (let i = 0; i<4; i++){
+            let td  = document.createElement("td")
+            td.textContent = "ahoj"
+            tr.appendChild(td)
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+    document.body.appendChild(table)
+
+
